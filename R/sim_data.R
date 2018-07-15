883c27765e630778e4fdc678b43237594b36e875
ozgurasarstat <ozgurasarstat@gmail.com>
2018-07-15 11:50:16 +0300

    sim_data document

---
+++ R/sim_data.R
@@ -0,0 +1,15 @@
+#' Simulated data-set.
+#'
+#' A dataset containing three-level ordinal outcomes
+#' for 250 clusters, 1000 subjects, 4 time points
+#'c_id s_id t        cov y
+#' @format A data frame with 4000 rows and 5 variables:
+#' \describe{
+#'   \item{c_id}{cluster id}
+#'   \item{s_id}{subject id}
+#'   \item{t}{time}
+#'   \item{cov}{covariate}
+#'   \item{y}{outcome}
+#'   ...
+#' }
+"sim_data"
